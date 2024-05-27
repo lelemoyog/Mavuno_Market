a9bc72f7f067fcd46a27165cfb6835916c3747fc
kelvincharlow <kelvincharlow78@gmail.com>
2024-05-27 09:57:03 +0300

    descprition

--- Mavuno_Market/MavunoMarketApp/urls.py
+++ Mavuno_Market/MavunoMarketApp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('home/', views.home, name='home'),
-  
+    path('descprition/', views.description, name='description'),
 
 ]
