5731832b00aa4dca06fbc34f6444ebe1c3b8a1c1
CarolineMuthoniThomas <caro.muthoni2012@gmail.com>
2024-05-27 08:49:48 +0300

    farmers

--- Mavuno_Market/MavunoMarketApp/urls.py
+++ Mavuno_Market/MavunoMarketApp/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('home/', views.home, name='home'),
-  
+    path('farmer/', views.farmer, name='farmer'),
+    path('login/', views.login_view, name='login'),  # Add login view
+    path('signup/', views.signup_view, name='signup'),  # Add signup view
 
 ]
+
--- Mavuno_Market/MavunoMarketApp/views.py
+++ Mavuno_Market/MavunoMarketApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render # type: ignore
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 
@@ -14,3 +16,24 @@ def contact(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def farmer(request):
+    return render(request, 'farmer.html')
+
+def login_view(request):
+    # Implement your login view logic here
+    return render(request, 'login_signup.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'login_signup.html', {'form': form})
