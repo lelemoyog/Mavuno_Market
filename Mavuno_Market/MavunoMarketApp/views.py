e0cbf1a26b217ea87ced97018bb5f6c12e177a38
CarolineMuthoniThomas <caro.muthoni2012@gmail.com>
2024-05-28 12:30:48 +0300

    farmers' card design

--- Mavuno_Market/MavunoMarketApp/views.py
+++ Mavuno_Market/MavunoMarketApp/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render # type: ignore
+
 
 
 
@@ -20,20 +19,5 @@ def home(request):
 def farmer(request):
     return render(request, 'farmer.html')
 
-def login_view(request):
-    # Implement your login view logic here
-    return render(request, 'login_signup.html')
-
-def signup_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'login_signup.html', {'form': form})
+def description(request):
+    return render(request, 'description.html')
