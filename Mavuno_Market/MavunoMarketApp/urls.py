073f468b5780ef49419e4701e9c72ebebc5e88b6
kelvincharlow <kelvincharlow78@gmail.com>
2024-06-07 10:10:57 +0300

    testimonial page

--- Mavuno_Market/MavunoMarketApp/urls.py
+++ Mavuno_Market/MavunoMarketApp/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     path('farmer/', views.farmer,name='farmer' ),
     path('profile/', views.profile, name='profile'),
     path('vendor/', views.vendor,name='vendor' ),
-    path('description/', views.description, name='description'),
+    
     path('testimonial/', views.testimonial, name='testimonial'),
+
+    path('description/', views.description, name='description'),
 ]
 
 
