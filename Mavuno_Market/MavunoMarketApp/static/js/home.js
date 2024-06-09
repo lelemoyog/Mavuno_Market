847bc5baa1eb460f79f9ae606b53cd5dc4ad739d
Lelemoyog <issasaitemu@gmail.com>
2024-06-09 20:56:49 +0300

    added search function

--- Mavuno_Market/MavunoMarketApp/static/js/home.js
+++ Mavuno_Market/MavunoMarketApp/static/js/home.js
@@ -51,24 +51,113 @@ $("#logOut").click(function (event) {
   });
 });
 
-//fetch products
-{/* <div class="col-md-6 col-lg-4 col-xl-3">
-<div class="rounded position-relative fruite-item">
-    <div class="fruite-img">
-        <img src="{% static 'img/fruite-item-1.jpg' %}" class="img-fluid w-100 rounded-top" alt="">
-    </div>
-    <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">Fruits</div>
-    <div class="p-4 border border-secondary border-top-0 rounded-bottom">
-        <h4>Oranges</h4>
-        <div class="d-flex justify-content-between flex-lg-wrap">
-            <p class="text-dark fs-5 fw-bold mb-0">Ksh 150 / kg</p>
-            <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> View Description</a>
-        </div>
-    </div>
-</div>
-</div> */}
+
 
 var id;
+
+//search function for products
+
+$("#search").click(function (event) {
+  event.preventDefault();
+  var search = document.getElementById('searchInput').value;
+  console.log(search);
+  clearBox();
+  //use firebase firestore search function to search for products with the name that matches the search input
+
+  getDocs(query(collection(db, "products"), where("name", "==", search), limit(8))).then(docSnap => {
+    let Products = [];
+    docSnap.forEach((doc) => {
+      Products.push({ ...doc.data(), id: doc.id })
+    });
+    console.log("Document data:", Products);
+    let goods = Products.length;
+    console.log(Products);
+    const veiwGoods = document.querySelector("#productHolder");
+   
+    for (let i = 0; i < goods; i++) {
+      var name  = Products[i]['name'];
+      var price = Products[i]['price'];
+      var category = Products[i]['category'];
+      var imgUrl = Products[i]['imgUrl'];
+      id = Products[i]['id'];
+      
+      var product = document.createElement("div");
+      product.className = "col-md-6 col-lg-4 col-xl-3";
+      
+      var fruiteItem = document.createElement("div");
+      fruiteItem.className = "rounded position-relative fruite-item";
+
+      var fruiteImg = document.createElement("div");
+      fruiteImg.className = "fruite-img";
+
+      var img = document.createElement("img");
+      img.src = imgUrl;
+      img.className = "img-fluid w-100 rounded-top";
+      img.alt = name;
+
+      
+
+      var textWhite = document.createElement("div");
+      textWhite.className = "text-white bg-success px-3 py-1 rounded position-absolute";
+      textWhite.style.top = "10px";
+      textWhite.style.left = "10px";
+      textWhite.innerHTML = category;
+
+      var border = document.createElement("div");
+      border.className = "p-4 border border-secondary border-top-0 rounded-bottom";
+
+      var h4 = document.createElement("h4");
+      h4.innerHTML = name;
+
+      var h6 = document.createElement("h6");
+      h6.innerHTML = id;
+      h6.style.display = "none";
+
+      var dFlex = document.createElement("div");
+      dFlex.className = "d-flex justify-content-between flex-lg-wrap";
+
+      var p = document.createElement("p");
+      p.className = "text-dark fs-5 fw-bold mb-0";
+      p.innerHTML = `Ksh ${price} / kg`;
+
+      var a = document.createElement("a");
+      a.href = "#";
+      a.className = "btn border border-secondary rounded-pill px-3 text-primary";
+      a.innerHTML = `<i class="fa fa-shopping-bag me-2 text-primary"></i> View Description`;
+
+      
+
+      veiwGoods.appendChild(product);
+      product.appendChild(fruiteItem);
+      fruiteItem.appendChild(fruiteImg);
+      fruiteImg.appendChild(img);
+      fruiteItem.appendChild(textWhite);
+      fruiteItem.appendChild(border);
+      border.appendChild(h4);
+      border.appendChild(h6);
+      border.appendChild(dFlex);
+      dFlex.appendChild(p);
+      dFlex.appendChild(a);
+
+      // Add event listener to the button
+      for (let i = 0; i < goods; i++) {
+        // ... existing code ...
+
+        (function(id, name) {
+          a.addEventListener('click', function() {
+            localStorage.setItem('productId', id);
+            console.log(name);
+            window.location.href = "/description/";
+          });
+        })(id, name);
+      }
+
+
+    }
+  });
+});
+
+
 
 function fetchProducts(){
   clearBox();
